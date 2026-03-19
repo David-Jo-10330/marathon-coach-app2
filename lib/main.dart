0785c2937cf46f3b3933de26b62c33712d336b0c
David-Jo-10330 <enjoyall2@gmail.com>
2026-03-19 17:43:28 +0900

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: '마라톤 코치',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late WebViewController _controller;
+
+  Future<bool> _onWillPop(BuildContext context) async {
+    if (await _controller.canGoBack()) {
+      _controller.goBack();
+      return Future.value(false);
+    } else {
+      return Future.value(true);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () => _onWillPop(context),
+      child: Scaffold(
+        body: SafeArea(
+          child: WebView(
+          // TODO: 여기에 방금 배포한 Streamlit 웹 주소 입력 (예: http://your-app-url.com)
+          initialUrl: "https://marathon-coach.streamlit.app",
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController webviewcontroller){
+            _controller = webviewcontroller;
+          },
+        ),
+        ),
+      ),
+    );
+  }
+}
